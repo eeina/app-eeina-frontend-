b053692b40772b9feb187eaad191ac9641e02812
takwaibrahimmohamed <takwaibrahimmohamed2002@gmail.com>
2026-01-04 13:42:49 +0300

    feat: update footer links and logo, enhance header component, and refine routing structure

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,65 +1,65 @@
-import { createBrowserRouter } from 'react-router-dom';
-import ProtectedRoute from '@/routes/ProtectedRoute';
-import { MainLayout, AuthLayout } from '@/components/Layout';
-import { AnalyticsLayout } from '@/components/Layout/AnalyticsLayout';
+import { createBrowserRouter } from "react-router-dom";
+import ProtectedRoute from "@/routes/ProtectedRoute";
+import { MainLayout, AuthLayout } from "@/components/Layout";
+import { AnalyticsLayout } from "@/components/Layout/AnalyticsLayout";
 
 // --- Pages ---
-import { ListsLayout } from '@/pages/ShoppingList/ListLayout';
-import { ListDetails } from '@/pages/ShoppingList/ListDetails';
-import { AccountSettings } from '@/pages/AccountSettings';
-import { Login, Signup } from '@/pages/Auth';
-import { OAuthCallback } from '@/pages/Auth/OAuthCallback';
-import { VerifyOtp } from '@/pages/Auth/VerifiOtp';
-import { Category } from '@/pages/Category';
-import { CreateRecipe } from '@/pages/CreateRecipe';
-import { EditRecipe } from '@/pages/EditRecipe/EditRecipe';
-import { Explore } from '@/pages/Explore';
-import { Home } from '@/pages/Home';
-import { Ingredient } from '@/pages/Ingredient/Ingredient';
-import { IngredientDetails } from '@/pages/IngredientDetails';
-import { MealPlanner } from '@/pages/MealPlanner';
-import NotFound from '@/pages/NotFound';
-import { Privacy_Policy } from '@/pages/PrivacyPolicy';
-import { ProcessedFood } from '@/pages/processedFood';
-import { ProcessedFoodDetails } from '@/pages/ProcessedFoodDetails';
-import CreatedByProfile from '@/pages/Profile/CreatedByProfile';
-import { RecipeDetails } from '@/pages/RecipeDetails';
-import { SearchRecipesResult } from '@/pages/SearchRecipesResult';
-import { Terms_Conditions } from '@/pages/Terms_Conditions';
-import { Trending } from '@/pages/Trending';
-import Checkout from '@/pages/Checkout/Checkout';
-import ProfileSetup from '@/pages/ProfileSetup/ProfileSetup';
-import GoalsSetup from '@/pages/Goals/GoalsSetup';
-import GoalsDashboard from '@/pages/Goals/GoalsDashboard';
-import GoalDashboardLayout from '@/components/Layout/GoalDashboardLayout';
-import DashboardMealHistory from '@/pages/Goals/DashboardMealHistory';
-import DashboardHealthSettings from '@/pages/Goals/DashboardHealthSettings';
-import ProfileLayOut from '@/components/Layout/ProfileLayoutv2.tsx/ProfileLayout';
-import Profile from '@/pages/Profilev2/Profile';
-import EditProfile from '@/pages/Profilev2/EditProfile';
-import EditProfileImage from '@/pages/Profilev2/component/editprofile/ProfileImages';
-import BasicInformation from '@/pages/Profilev2/component/editprofile/BasicInformation';
-import SocialMediaLinks from '@/pages/Profilev2/component/editprofile/SocialMediaLinks';
-import HealthProfile from '@/pages/Profilev2/component/editprofile/HealthProfile';
-import Preferences from '@/pages/Profilev2/component/editprofile/Preferencers';
-import ProfileGoalsLayout from '@/pages/Profilev2/component/goals/ProfileGoalsLayout';
-import ProfileViewGoals from '@/pages/Profilev2/component/goals/ProfileViewGoals';
-import ProfileMealHistory from '@/pages/Profilev2/component/goals/ProfileMealHistory';
-import ProfileHealthSettings from '@/pages/Profilev2/component/goals/ProfileHealthSettings';
-import Packages from '@/pages/Packages/Packages';
-import OrderSummary from '@/pages/OrderSummary/OrderSummary';
-import PaymentPage from '@/pages/Payment/PaymentPage';
-import PaymentSuccess from '@/pages/Payment/PaymentSuccess';
-import Nutritionist from '@/pages/Nutritionist/Nutritionist';
-import NutritionistVideo from '@/pages/Nutritionist/NutritionistVideo';
-import NutritionistChat from '@/pages/Nutritionist/NutritionistChat';
-import NutritionistLayout from '@/components/Layout/NutritionistLayout';
-import Saved from '@/pages/Profilev2/component/Saved';
-import PaymentCallback from '@/pages/Payment/PaymentCallback';
-import PaymentFailed from '@/pages/Payment/PaymentFailed';
-import SubscriptionPage from '@/pages/Subscription/SubscriptionPage';
-import Forgetpassword from '@/pages/Auth/Forgetpassword';
-import Resetpassword from '@/pages/Auth/Resetpassword';
+import { ListsLayout } from "@/pages/ShoppingList/ListLayout";
+import { ListDetails } from "@/pages/ShoppingList/ListDetails";
+import { AccountSettings } from "@/pages/AccountSettings";
+import { Login, Signup } from "@/pages/Auth";
+import { OAuthCallback } from "@/pages/Auth/OAuthCallback";
+import { VerifyOtp } from "@/pages/Auth/VerifiOtp";
+import { Category } from "@/pages/Category";
+import { CreateRecipe } from "@/pages/CreateRecipe";
+import { EditRecipe } from "@/pages/EditRecipe/EditRecipe";
+import { Explore } from "@/pages/Explore";
+import { Home } from "@/pages/Home";
+import { Ingredient } from "@/pages/Ingredient/Ingredient";
+import { IngredientDetails } from "@/pages/IngredientDetails";
+import { MealPlanner } from "@/pages/MealPlanner";
+import NotFound from "@/pages/NotFound";
+import { Privacy_Policy } from "@/pages/PrivacyPolicy";
+import { ProcessedFood } from "@/pages/processedFood";
+import { ProcessedFoodDetails } from "@/pages/ProcessedFoodDetails";
+import CreatedByProfile from "@/pages/Profile/CreatedByProfile";
+import { RecipeDetails } from "@/pages/RecipeDetails";
+import { SearchRecipesResult } from "@/pages/SearchRecipesResult";
+import { Terms_Conditions } from "@/pages/Terms_Conditions";
+import { Trending } from "@/pages/Trending";
+import Checkout from "@/pages/Checkout/Checkout";
+import ProfileSetup from "@/pages/ProfileSetup/ProfileSetup";
+import GoalsSetup from "@/pages/Goals/GoalsSetup";
+import GoalsDashboard from "@/pages/Goals/GoalsDashboard";
+import GoalDashboardLayout from "@/components/Layout/GoalDashboardLayout";
+import DashboardMealHistory from "@/pages/Goals/DashboardMealHistory";
+import DashboardHealthSettings from "@/pages/Goals/DashboardHealthSettings";
+import ProfileLayOut from "@/components/Layout/ProfileLayoutv2.tsx/ProfileLayout";
+import Profile from "@/pages/Profilev2/Profile";
+import EditProfile from "@/pages/Profilev2/EditProfile";
+import EditProfileImage from "@/pages/Profilev2/component/editprofile/ProfileImages";
+import BasicInformation from "@/pages/Profilev2/component/editprofile/BasicInformation";
+import SocialMediaLinks from "@/pages/Profilev2/component/editprofile/SocialMediaLinks";
+import HealthProfile from "@/pages/Profilev2/component/editprofile/HealthProfile";
+import Preferences from "@/pages/Profilev2/component/editprofile/Preferencers";
+import ProfileGoalsLayout from "@/pages/Profilev2/component/goals/ProfileGoalsLayout";
+import ProfileViewGoals from "@/pages/Profilev2/component/goals/ProfileViewGoals";
+import ProfileMealHistory from "@/pages/Profilev2/component/goals/ProfileMealHistory";
+import ProfileHealthSettings from "@/pages/Profilev2/component/goals/ProfileHealthSettings";
+import Packages from "@/pages/Packages/Packages";
+import OrderSummary from "@/pages/OrderSummary/OrderSummary";
+import PaymentPage from "@/pages/Payment/PaymentPage";
+import PaymentSuccess from "@/pages/Payment/PaymentSuccess";
+import Nutritionist from "@/pages/Nutritionist/Nutritionist";
+import NutritionistVideo from "@/pages/Nutritionist/NutritionistVideo";
+import NutritionistChat from "@/pages/Nutritionist/NutritionistChat";
+import NutritionistLayout from "@/components/Layout/NutritionistLayout";
+import Saved from "@/pages/Profilev2/component/Saved";
+import PaymentCallback from "@/pages/Payment/PaymentCallback";
+import PaymentFailed from "@/pages/Payment/PaymentFailed";
+import SubscriptionPage from "@/pages/Subscription/SubscriptionPage";
+import Forgetpassword from "@/pages/Auth/Forgetpassword";
+import Resetpassword from "@/pages/Auth/Resetpassword";
 
 // ------------------------------------------------------
 // ENGLISH ROUTES
@@ -69,21 +69,21 @@ const englishRoutes = [
   {
     element: <AuthLayout />,
     children: [
-      { path: '/login', element: <Login /> },
-      { path: '/signup/*', element: <Signup /> },
-      { path: '/verify-otp', element: <VerifyOtp /> },
-      { path: '/forgot-password', element: <Forgetpassword /> },
-      { path: '/Resetpassword', element: <Resetpassword /> },
+      { path: "/login", element: <Login /> },
+      { path: "/signup/*", element: <Signup /> },
+      { path: "/verify-otp", element: <VerifyOtp /> },
+      { path: "/forgot-password", element: <Forgetpassword /> },
+      { path: "/Resetpassword", element: <Resetpassword /> },
       {
-        path: '/goals-setup',
+        path: "/goals-setup",
         element: (
           <ProtectedRoute>
             <GoalsSetup />
           </ProtectedRoute>
         ),
       },
-      { path: '/Profile-Setup', element: <ProfileSetup /> },
-      { path: '/auth/callback', element: <OAuthCallback /> },
+      { path: "/Profile-Setup", element: <ProfileSetup /> },
+      { path: "/auth/callback", element: <OAuthCallback /> },
     ],
   },
 
@@ -91,32 +91,32 @@ const englishRoutes = [
   {
     element: <MainLayout />,
     children: [
-      { path: '/', element: <Home /> },
-      { path: '/recipes', element: <Explore /> },
-      { path: '/processed-food', element: <ProcessedFood /> },
-      { path: '/processed-food/:slug', element: <ProcessedFoodDetails /> },
-      { path: '/ingredients', element: <Ingredient /> },
-      { path: '/ingredient/:slug', element: <IngredientDetails /> },
-      { path: '/recipe/:slug', element: <RecipeDetails /> },
-      { path: '/category/:categoryName', element: <Category /> },
-      { path: '/user/:id', element: <CreatedByProfile /> },
-      { path: '/trending', element: <Trending /> },
-      { path: '/privacy-policy', element: <Privacy_Policy /> },
-      { path: '/Terms_Conditions', element: <Terms_Conditions /> },
-      { path: '/search', element: <SearchRecipesResult /> },
+      { path: "/", element: <Home /> },
+      { path: "/recipes", element: <Explore /> },
+      { path: "/processed-food", element: <ProcessedFood /> },
+      { path: "/processed-food/:slug", element: <ProcessedFoodDetails /> },
+      { path: "/ingredients", element: <Ingredient /> },
+      { path: "/ingredient/:slug", element: <IngredientDetails /> },
+      { path: "/recipe/:slug", element: <RecipeDetails /> },
+      { path: "/category/:categoryName", element: <Category /> },
+      { path: "/user/:id", element: <CreatedByProfile /> },
+      { path: "/trending", element: <Trending /> },
+      { path: "/privacy-policy", element: <Privacy_Policy /> },
+      { path: "/Terms_Conditions", element: <Terms_Conditions /> },
+      { path: "/search", element: <SearchRecipesResult /> },
 
-      { path: '/packages', element: <Packages /> },
-      { path: '/order-summary/:orderId', element: <OrderSummary /> },
-      { path: '/payment/:orderId', element: <PaymentPage /> },
-      { path: '/payment/callback/:orderId', element: <PaymentCallback /> },
-      { path: '/payment/success/:orderId', element: <PaymentSuccess /> },
-      { path: '/payment/failed/:orderId', element: <PaymentFailed /> },
-      { path: '/subscription', element: <SubscriptionPage /> },
+      { path: "/packages", element: <Packages /> },
+      { path: "/order-summary/:orderId", element: <OrderSummary /> },
+      { path: "/payment/:orderId", element: <PaymentPage /> },
+      { path: "/payment/callback/:orderId", element: <PaymentCallback /> },
+      { path: "/payment/success/:orderId", element: <PaymentSuccess /> },
+      { path: "/payment/failed/:orderId", element: <PaymentFailed /> },
+      { path: "/subscription", element: <SubscriptionPage /> },
 
       // Protected Routes
 
       {
-        path: '/planner',
+        path: "/planner",
         element: (
           <ProtectedRoute>
             <MealPlanner />
@@ -124,17 +124,21 @@ const englishRoutes = [
         ),
       },
       {
-        path: '/lists',
-        element: <ListsLayout />,
+        path: "/lists",
+        element: (
+          <ProtectedRoute>
+            <ListsLayout />
+          </ProtectedRoute>
+        ),
         children: [
           {
-            path: ':id',
+            path: ":id",
             element: <ListDetails />,
           },
         ],
       },
       {
-        path: '/create-recipe',
+        path: "/create-recipe",
         element: (
           <ProtectedRoute>
             <CreateRecipe />
@@ -142,7 +146,7 @@ const englishRoutes = [
         ),
       },
       {
-        path: '/edit-recipe/:id',
+        path: "/edit-recipe/:id",
         element: (
           <ProtectedRoute>
             <EditRecipe />
@@ -150,7 +154,7 @@ const englishRoutes = [
         ),
       },
       {
-        path: '/checkout',
+        path: "/checkout",
         element: (
           <ProtectedRoute>
             <Checkout />
@@ -159,7 +163,7 @@ const englishRoutes = [
       },
 
       // 404 Fallback
-      { path: '*', element: <NotFound /> },
+      { path: "*", element: <NotFound /> },
     ],
   },
 
@@ -167,7 +171,7 @@ const englishRoutes = [
     element: <ProfileLayOut />,
     children: [
       {
-        path: '/Profile',
+        path: "/Profile",
         element: (
           <ProtectedRoute>
             <Profile />
@@ -175,7 +179,7 @@ const englishRoutes = [
         ),
       },
       {
-        path: '/Profile/goals',
+        path: "/Profile/goals",
         element: (
           <ProtectedRoute>
             <ProfileGoalsLayout />
@@ -184,35 +188,35 @@ const englishRoutes = [
         children: [
           {
             index: true,
-            element: <ProfileViewGoals />
+            element: <ProfileViewGoals />,
           },
           {
-            path: 'meal-history',
-            element: <ProfileMealHistory />
+            path: "meal-history",
+            element: <ProfileMealHistory />,
           },
           {
-            path: 'health-settings',
-            element: <ProfileHealthSettings />
-          }
-        ]
+            path: "health-settings",
+            element: <ProfileHealthSettings />,
+          },
+        ],
       },
       {
-        path: '/edit',
+        path: "/edit",
         element: (
           <ProtectedRoute>
             <EditProfile />
           </ProtectedRoute>
         ),
         children: [
-          { path: 'Profile-images', element: <EditProfileImage /> },
-          { path: 'Basic-information', element: <BasicInformation /> },
-          { path: 'Preferencers', element: <Preferences /> },
-          { path: 'Social-Media', element: <SocialMediaLinks /> },
-          { path: 'Health-Profile', element: <HealthProfile /> },
+          { path: "Profile-images", element: <EditProfileImage /> },
+          { path: "Basic-information", element: <BasicInformation /> },
+          { path: "Preferencers", element: <Preferences /> },
+          { path: "Social-Media", element: <SocialMediaLinks /> },
+          { path: "Health-Profile", element: <HealthProfile /> },
         ],
       },
       {
-        path: '/savedrecipes',
+        path: "/savedrecipes",
         element: (
           <ProtectedRoute>
             <Saved />
@@ -220,7 +224,7 @@ const englishRoutes = [
         ),
       },
       {
-        path: '/account-settings',
+        path: "/account-settings",
         element: (
           <ProtectedRoute>
             <AccountSettings />
@@ -228,7 +232,7 @@ const englishRoutes = [
         ),
       },
       {
-        path: '/subscription',
+        path: "/subscription",
         element: (
           <ProtectedRoute>
             <SubscriptionPage />
@@ -243,7 +247,7 @@ const englishRoutes = [
     element: <GoalDashboardLayout />,
     children: [
       {
-        path: '/goals-dashboard',
+        path: "/goals-dashboard",
         element: (
           <ProtectedRoute>
             <GoalsDashboard />
@@ -251,7 +255,7 @@ const englishRoutes = [
         ),
       },
       {
-        path: '/dashboard-meal-history',
+        path: "/dashboard-meal-history",
         element: (
           <ProtectedRoute>
             <DashboardMealHistory />
@@ -259,7 +263,7 @@ const englishRoutes = [
         ),
       },
       {
-        path: '/dashboard-health-settings',
+        path: "/dashboard-health-settings",
         element: (
           <ProtectedRoute>
             <DashboardHealthSettings />
@@ -273,15 +277,15 @@ const englishRoutes = [
     element: <NutritionistLayout />,
     children: [
       {
-        path: '/nutritionist',
+        path: "/nutritionist",
         element: <Nutritionist />,
       },
       {
-        path: '/nutritionist/video',
+        path: "/nutritionist/video",
         element: <NutritionistVideo />,
       },
       {
-        path: '/nutritionist/chat',
+        path: "/nutritionist/chat",
         element: <NutritionistChat />,
       },
     ],
